3955c714796ce97c216c2c3424838fc1551f961f
Muditam Leads <madhur.muditam@gmail.com>
2025-02-08 17:00:13 +0530

    cors changes revert

--- server.js
+++ server.js
@@ -15,19 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// app.use(cors());
-
-const allowedOrigins = ['http://localhost:3000', 'https://60brands.com'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-}));
-
+app.use(cors());
 app.use(express.json());
 
 
